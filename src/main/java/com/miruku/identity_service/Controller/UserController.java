d9b1064e123fb201077214da747c185b366e379c
Miruku2201 <miruku2201@gmail.com>
2024-06-01 11:40:48 +0700

    Service: get all users, get 1 user, delete user, update user

--- src/main/java/com/miruku/identity_service/Controller/UserController.java
+++ src/main/java/com/miruku/identity_service/Controller/UserController.java
@@ -3,11 +3,11 @@ package com.miruku.identity_service.Controller;
 import com.miruku.identity_service.Entity.User;
 import com.miruku.identity_service.Service.UserService;
 import com.miruku.identity_service.dto.Request.UserCreationRequest;
+import com.miruku.identity_service.dto.Request.UserUpdateRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -18,5 +18,25 @@ public class UserController {
     @PostMapping
      User createUser(@RequestBody UserCreationRequest request){
         return userService.createUser(request);
+    }
+
+    @GetMapping
+    List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId){
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable("userId") String userId, @RequestBody UserUpdateRequest request){
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    void deleteUser(@PathVariable("userId") String userId){
+        userService.deleteUser(userId);
     }
 }
--- src/main/java/com/miruku/identity_service/Service/UserService.java
+++ src/main/java/com/miruku/identity_service/Service/UserService.java
@@ -3,9 +3,12 @@ package com.miruku.identity_service.Service;
 import com.miruku.identity_service.Entity.User;
 import com.miruku.identity_service.Repository.UserRepository;
 import com.miruku.identity_service.dto.Request.UserCreationRequest;
+import com.miruku.identity_service.dto.Request.UserUpdateRequest;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -19,5 +22,25 @@ public class UserService {
         user.setUsername(request.getUsername());
         user.setPassword(request.getPassword());
         return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUser(String userId){
+        return userRepository.findById(userId).orElseThrow(()-> new RuntimeException("No id valid"));
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request){
+        User targetUser = getUser(userId);
+        targetUser.setPassword(request.getPassword());
+        targetUser.setName(request.getName());
+        targetUser.setUsername(request.getUsername());
+        return userRepository.save(targetUser);
+    }
+
+    public void deleteUser(String userId){
+        userRepository.deleteById(userId);
     }
 }
